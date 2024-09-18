2e42c6a31d38cee389b33ce7aadbe86397c64b3c
plastpase123 <linus.hellstrm@hotmail.com>
2024-09-18 13:39:01 +0200

    SoE

--- Assignement2/SoE.cpp
+++ Assignement2/SoE.cpp
@@ -1,12 +1,19 @@
 // Program that finds prime numbers between 1 and an arbitrary number Max
 #include <iostream>
-#include <thread>
 #include <mutex>
 #include <list>
 #include <vector>
 #include <cmath>
 #include <bits/stdc++.h>
+#include <thread>
 
+std::mutex lock;
+
+void usage(char *program)
+{
+    std::cout << "Usage: " << program << " max threads, 1 <= max,  1 < threads <= max" << std::endl;
+    exit(1);
+}
 
 void print_container(const std::vector<int>& c)
 {
@@ -28,14 +35,34 @@ void compute_primes(int k, int max, std::vector<int>& list){
       k++;
     }
   }
-
 }
+
+void compute_primes_parallell(int start, int max, std::vector<int>& primes, std::vector<int>& list){
+  for (int k : primes){
+    for (int i = start; i <= max; i++){
+      if (list[i-1] % k == 0){
+        list[i-1] = 0;
+      }
+    }
+  }
+}
+
+
 
 int main(int argc, char*argv[]){
 
-  int max = std::stoi(argv[1]);
+  if (argc != 3){
+    usage(argv[0]);
+  }
 
+  int max = std::stoi(argv[1]);
+  int sqrtmax = sqrt(max);
+  int elements = max - sqrtmax;
+  int threads = std::stoi(argv[2]);
+  int chunk_size = elements / threads;
   std::vector<int> list;
+
+
 
   for (int i = 1; i <= max; i++){
     list.push_back(i);
@@ -45,33 +72,43 @@ int main(int argc, char*argv[]){
   list[0] = 0;
 
 
-
-// Compute std::round(sqrt(max))
-//
-// Given p threads, build p chunks covering range from sqrt(max) + 1 to max
-// Numbers to cover = max - (sqrt(max) + 1)
-// Numbers for thread0 = Numbers to cover / p + rest
-// Numbers per thread = Numbers to cover / p
-//
-
-  //int sqrtmax = std::round(sqrt(max));
-  // int span_length = max - sqrtmax + 1;
-  // int span_per_thread = span_length / p;
-  //
-
-  compute_primes(k, max, list);
-
+  compute_primes(k, sqrtmax, list);
 
   std::vector<int> primes;
 
   //  Retrieve primes from original list
-  for (int i: list){
-    if (i != 0){
-        primes.push_back(i);
+  for(int i = 0; i < sqrtmax; i++){
+    if (list[i] != 0){
+        primes.push_back(list[i]);
       }
   }
 
-  print_container(primes);
+  int start = sqrtmax + 1;
 
+  auto start_time = std::chrono::system_clock::now();
+
+
+  std::thread *t = new std::thread[threads];
+  for (int i = 0; i < threads; i++){
+    if (i != threads - 1){
+      t[i] = std::thread(compute_primes_parallell, start*(i+1), start*(i+1) + chunk_size, std::ref(primes), std::ref(list));
+    }
+    else {
+      compute_primes_parallell(start*(i+1), max, primes, list);
+    }
+  }
+
+  for(int i = start; i < max; i++){
+    if (list[i] != 0){
+        primes.push_back(list[i]);
+      }
+  }
+
+  std::chrono::duration<double> duration =
+        (std::chrono::system_clock::now() - start_time);
+  // *** timing ends here ***
+  std::cout << "Finished in " << duration.count() << " seconds (wall clock)." << std::endl;
+
+  //print_container(primes);
   return 0;
 }
