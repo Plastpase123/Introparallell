b29f5417b4c3a830708a06f5fa96df498f110358
rofo8072 <robin.forslund.8072@student.uu.se>
2024-09-30 11:16:26 +0200

    Add benchmark automation

---
+++ Assignement2/sorted_list_benchmark.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+#!/bin/bash
+
+# File to store the results
+output_file="results_sorted_list.txt"
+
+# Clear the results file if it exists, or create it if it doesn't
+> "$output_file"
+
+# Array of values for threads
+tvalues=(1 2 4 8 16 32)
+
+# Iterate over all combinations of w and threads
+for t in "${tvalues[@]}"; do
+    # Run the command and append the output to the file
+    echo "Running ./benchmark_example.o with threads=$t"
+    result=$(./benchmark_example.o $w $t $o)
+    echo "w=$w, threads=$t, Result:$result" >> "$output_file"
+done
+
+echo "All results have been written to $output_file"
