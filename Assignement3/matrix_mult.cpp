3d1991d530efe82239b7eebcc82356d85f78e154
rofo8072 <robin.forslund.8072@student.uu.se>
2024-10-10 16:31:18 +0200

    test

--- Assignement3/matrix_mult.cpp
+++ Assignement3/matrix_mult.cpp
@@ -57,9 +57,22 @@ std::vector<std::vector<int>> multiply_3_collapse(int dim, std::vector<std::vect
     return c;
 }
 
-std::vector<std::vector<int>> zero_matrix(int dim)
+std::vector<std::vector<int>> fill_in_matrix(int dim, std::vector<std::vector<int>> &matrix)
 {
-    std::vector<std::vector<int>> c(dim, std::vector<int>(dim));
+    srand(time(0));
+    // Fill the matrix with random integers
+    for (int i = 0; i < dim; ++i)
+    {
+        for (int j = 0; j < dim; ++j)
+        {
+            matrix[i][j] = std::rand() % 10; // Random integers between 0 and 99
+        }
+    }
+    return c;
+}
+
+std::vector<std::vector<int>> zero_matrix(int dim, std::vector<std::vector<int>> &c)
+{
     for (int i = 0; i < dim; ++i)
     {
         for (int j = 0; j < dim; ++j)
@@ -69,63 +82,59 @@ std::vector<std::vector<int>> zero_matrix(int dim)
     }
     return c;
 }
-    void usage(char *program)
+
+void usage(char *program)
+{
+    std::cout << "Usage: " << program << " dim threads" << std::endl;
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+
+    if (argc != 2)
     {
-        std::cout << "Usage: " << program << " dim threads" << std::endl;
-        exit(1);
+        usage(argv[0]);
     }
 
-    int main(int argc, char *argv[])
+    int dim = std::atoi(argv[1]);
+    // int nthrds = std::atoi(argv[2]);
+
+    std::vector<std::vector<int>> a(dim, std::vector<int>(dim));
+    std::vector<std::vector<int>> b(dim, std::vector<int>(dim));
+    std::vector<std::vector<int>> c = zero_matrix(dim);
+
+    a = fill_in_matrix(dim, a);
+    b = fill_in_matrix(dim, b);
+
+    list<int> threads = {1, 2, 4, 8, 16, 32};
+
+    for (int i = 0; i < threads.size(); i++)
     {
+        omp_set_num_threads(threads[i]);
 
-        if (argc != 2)
-        {
-            usage(argv[0]);
-        }
-	
-	omp_set_num_threads(8);
+        c = zero_matrix(dim);
 
-        int dim = std::atoi(argv[1]);
-        // int nthrds = std::atoi(argv[2]);
+        double start_time = omp_get_wtime();
+        c = multiply_1_collapse(dim, a, b, c);
+        double stop_time = omp_get_wtime();
 
-        std::vector<std::vector<int>> a(dim, std::vector<int>(dim));
-        std::vector<std::vector<int>> b(dim, std::vector<int>(dim));
-        std::vector<std::vector<int>> c = zero_matrix(dim);
+        std::cout << "Time taken for 1 collapse: " << stop_time - start_time << ", with dim: " << dim << " and " << threads[i] << " threads" << std::endl;
 
-        srand(time(0));
-        // Fill the matrix with random integers
-        for (int i = 0; i < dim; ++i)
-        {
-            for (int j = 0; j < dim; ++j)
-            {
-                a[i][j] = std::rand() % 10; // Random integers between 0 and 99
-                b[i][j] = std::rand() % 10;
-            }
-        }
+        c = zero_matrix(dim);
+
+        double start_time = omp_get_wtime();
+        c = multiply_2_collapse(dim, a, b, c);
+        double stop_time = omp_get_wtime();
+        std::cout << "Time taken for 2 collapse: " << stop_time - start_time << ", with dim: " << dim << " and " << threads[i] << " threads" << std::endl;
+
+        c = zero_matrix(dim);
 
         double start_time = omp_get_wtime();
         c = multiply_3_collapse(dim, a, b, c);
         double stop_time = omp_get_wtime();
-        std::cout << "Time taken for 1 collapse: " << stop_time - start_time << std::endl;
-	
-
-	omp_set_num_threads(32);
-	c = zero_matrix(dim);
-	start_time = omp_get_wtime();
-	c = multiply_3_collapse(dim, a, b, c);
-	stop_time = omp_get_wtime();
-	std::cout << "Time taken for 3 collapse: " << stop_time - start_time << std::endl;
-    
-        // Output the matrix
-        /*
-        std::cout << "The " << dim << "x" << dim << " matrix is: \n";
-        for (int i = 0; i < dim; ++i) {
-          for (int j = 0; j < dim; ++j) {
-            std::cout << c[i][j] << " ";
-          }
-          std::cout << std::endl;
-        }*/
-
-        return 0;
+        std::cout << "Time taken for 3 collapse: " << stop_time - start_time << ", with dim: " << dim << " and " << threads[i] << " threads" << std::endl;
     }
 
+    return 0;
+}
