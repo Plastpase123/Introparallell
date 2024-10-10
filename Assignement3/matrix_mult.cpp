abd971f0034f89e19f68594ca52f7697725b17df
plastpase123 <linus.hellstrm@hotmail.com>
2024-10-10 14:06:59 +0200

    added matrix mult code, SoE benchmark

---
+++ Assignement3/matrix_mult.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <omp.h>
+#include <bits/stdc++.h>
+#include <vector>
+void usage(char *program) {
+  std::cout << "Usage: " << program << " dim threads" << std::endl;
+  exit(1);
+}
+
+
+
+int main(int argc, char *argv[]) {
+
+  if (argc != 3) {
+    usage(argv[0]);
+  }
+
+  int dim = std::atoi(argv[1]);
+  int nthrds = std::atoi(argv[2]);
+  std::vector<std::vector<int>> a(dim, std::vector<int>(dim));
+  std::vector<std::vector<int>> b(dim, std::vector<int>(dim));
+  std::vector<std::vector<int>> c(dim, std::vector<int>(dim));
+
+  srand(time(0));
+  // Fill the matrix with random integers
+  for (int i = 0; i < dim; ++i) {
+    for (int j = 0; j < dim; ++j) {
+      a[i][j] = std::rand() % 10;  // Random integers between 0 and 99
+      b[i][j] = std::rand() % 10;
+    }
+  }
+
+  auto start_time = std::chrono::system_clock::now();
+
+ /* static scheduling of matrix multiplication loops */
+#pragma omp parallel default(private) shared (a, b, c, dim) num_threads(nthrds)
+  #pragma omp for schedule(static)
+  for (int i = 0; i < dim; i++) {
+    for (int j = 0; j < dim; j++) {
+      c[i][j] = 0;
+      for (int k = 0; k < dim; k++) {
+        c[i][j] += a[i][k] * b[k][j];
+      }
+    }
+  }
+ std::chrono::duration<double> duration =
+      (std::chrono::system_clock::now() - start_time);
+  // *** timing ends here ***
+  std::cout << "Finished in " << duration.count() << " seconds (wall clock).\n";
+
+  // Output the matrix
+  /*
+  std::cout << "The " << dim << "x" << dim << " matrix is: \n";
+  for (int i = 0; i < dim; ++i) {
+    for (int j = 0; j < dim; ++j) {
+      std::cout << c[i][j] << " ";
+    }
+    std::cout << std::endl;
+  }*/
+
+  return 0;
+}
