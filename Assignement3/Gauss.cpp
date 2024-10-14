011639fde06260db324d33ef82ddb4e828fa1bf2
plastpase123 <linus.hellstrm@hotmail.com>
2024-10-14 14:49:55 +0200

    exercise 4

--- Assignement3/Gauss.cpp
+++ Assignement3/Gauss.cpp
@@ -34,6 +34,7 @@ void col_oriented(int n, std::vector<int> &x, std::vector<int> &b, std::vector<s
 void row_oriented(int n, std::vector<int> &x, std::vector<int> &b, std::vector<std::vector<int>> &A)
 {
 
+  #pragma omp parallel for collapse(1)  num_threads(8)
   for (int row = n - 1; row >= 0; row--)
   {
     x[row] = b[row];
@@ -52,6 +53,8 @@ std::vector<std::vector<int>> create_matrix(int dim)
   srand(time(0));
 
   std::vector<std::vector<int>> matrix(dim, std::vector<int>(dim));
+  //#pragma omp parallel for collapse(2) num_threads(4)
+
   for (int i = 0; i < dim; ++i)
   {
     for (int j = 0; j < dim; ++j)
@@ -66,11 +69,7 @@ std::vector<std::vector<int>> create_matrix(int dim)
 std::vector<int> create_vector(int dim)
 {
 
-  std::vector<int> vector(dim);
-  for (int i = 0; i < dim; ++i)
-  {
-    vector[i] = 0;
-  }
+  std::vector<int> vector(dim, 0);
 
   return vector;
 }
@@ -83,7 +82,7 @@ int main(int argc, char *argv[])
     usage(argv[0]);
   }
 
-  int dim = argc[1];
+  int dim = std::stoi(argv[1]);
 
   std::vector<int> x(dim, 0); // lhs
   // std::vector<int> b = {3, 7, 5}; // rhs
@@ -91,19 +90,26 @@ int main(int argc, char *argv[])
   //{4,  -5,  1},
   //{2,  -1, -3}};
 
-  // std::vector<int> b = {3, 1, 0}; // rhs
-  // std::vector<std::vector<int>> A = {{2, -3, 0},
+  //std::vector<int> b = {3, 1, 0}; // rhs
+  //std::vector<std::vector<int>> A = {{2, -3, 0},
   //                                    {0, 1, 1},
   //                                    {0, 0, -5}};
 
-  std::vector<int> b = create_vector(dim);              // rhs
+  std::vector<int> b = create_vector(dim);// rhs
   std::vector<std::vector<int>> A = create_matrix(dim); // lhs
 
+  double start_time, stop_time;
+  start_time = omp_get_wtime();
   row_oriented(dim, x, b, A);
+  stop_time = omp_get_wtime();
+
+
+  std::cout << "Time taken for 1 collapse: " << stop_time - start_time << ", with dim = " << dim << std::endl;
+  /*
   for (int i = 0; i < dim; i++)
   {
     std::cout << x[i] << "\n";
-  }
+  }*/
 
   return 0;
 }
